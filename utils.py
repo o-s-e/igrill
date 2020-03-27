66f28ccf1fbed731cfb1c76b9c5ff62d349a6010
o-s-e <ose@recommind.com>
2020-03-27 15:56:05 +0000

    changed mqtt output to influxdb format

--- utils.py
+++ utils.py
@@ -107,8 +107,8 @@ def publish(temperatures, battery, client, base_topic, device_name):
             logging.debug(time.time_ns())
             client.publish("{0}".format(base_topic),
                            "{0},probe={1} temperature={2} {3}".format(device_name, i, temperatures[i], time.time_ns()))
-
-    client.publish("{0}/{1}/battery".format(base_topic, device_name), battery)
+    client.publish("{0}".format(base_topic),
+                   "{0},battery charge={2} {3}".format(device_name, battery, time.time_ns()))
 
 
 def get_devices(device_config):
